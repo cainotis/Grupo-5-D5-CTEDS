cc244a6f2e249c71faa5d06cefb1f51bbebdb14b
Pedro <pedrovrs@usp.br>
2022-08-12 05:38:25 -0300

    Add comunicacao entre o db e o back end
    Fiz como "aplicação web" pq encontrei um tutorial
    Link:https://www.youtube.com/watch?v=yGwGLcqcJ6Q

---
+++ dbComunication/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DBWebApp
+{
+    public partial class Movie
+    {
+        public Movie()
+        {
+            Platforms = new HashSet<Platform>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public DateTime? UpdateDttm { get; set; }
+
+        public virtual ICollection<Platform> Platforms { get; set; }
+    }
+}
